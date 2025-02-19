2d5e4791ac160dd45e459366ba1f54a561ff2598
stevensuna <79344380+stevensuna@users.noreply.github.com>
2025-02-19 09:34:44 +0000

    refactor(vite): ♻️ remove Tailwind CSS plugin from Vite config

--- meet-landing-page/vite.config.ts
+++ meet-landing-page/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
   ],
   server: {
     host: '0.0.0.0',
